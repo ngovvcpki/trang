d98d670e3e7be06513e8bd3f0c0f8a8db88b495c
eJwtzMtqwzAQQNF/mbUWthPHkval0CZ90dI2G6HH2BWWLTGy44SQfy8Eby+cewVtLeb8GXscQcLGbIxo66IpuGtKzlFYIVzhtgJNtatsg7qozbYEBoniyTuklV7Xk5rWVfcX1fczvrVn/rJ7eB3eu9/6w+7L43LIP3z/+FSX1dfxcAIGd6KmS0KQYFATEjDINt6DdoMfZZpN8Fb1eGGE2slIHSNMkc0ZSeKgfWBLpL4NcQEGeE6eMCs9gRznEG63fwZST3k= <tutruongcu@gmail.com>
2024-10-23 21:55:42 +0700

    First commit

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+    introTitle: 'Cậu gì đó ơi <3',
+    introDesc: `Gieo mạ một sớm hừng đông
+Đến giờ lúa đã trổ bông ngoài đồng
+Theo cậu từ độ trăng hồng
+Đến giờ cậu đã phải lòng tớ chưa`,
+    btnIntro: 'hihi ',
+    title: 'Phải chăng cậu đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng Cậu đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu tớ hong :3',
+    btnNo: 'Không, Là bạn bè :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao Cậu lại yêu tớ <3',
+    btnReply: 'Gửi cho trang <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h chơi game nhó.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'http://fb.com/profile.php?id=100051067247889' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
